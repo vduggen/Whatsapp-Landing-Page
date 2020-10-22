2eeedc297caf90a3e342e0d86e45f104576e816c
vlduggen <zduggem@gmail.com>
2020-10-21 21:57:31 -0300

    Add favicon and title

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,15 @@ import Security from "../components/Security";
 import * as S from "../styles/pages/home";
 import Questions from "../components/Questions";
 import Footer from "../components/Footer";
+import Head from "next/head";
 
 function Home() {
   return (
     <>
+      <Head>
+        <title>Whatsapp Landing Page</title>
+      </Head>
+
       <S.Container>
         <Header />
 
